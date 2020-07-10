d9cece65c9843a0dbb1ddc5e6cb6d709a14ac9c9
Matt Reimer <matt.reimer@gmail.com>
2020-07-10 12:56:52 -0700

    [DEPLOY CLIENT]: v.0.1.23

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/net_att/precache-manifest.574b39bb317c0ab8d2613507b93027ed.js"
+  "/net_att/precache-manifest.5fb825b93af9db7e66d4f8bdf60d878d.js"
 );
 
 self.addEventListener('message', (event) => {
